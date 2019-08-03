817f3667a5fe31d012f10c5445646c8d819e9b00
Jiss James <jissjames360@gmail.com>
2019-08-03 13:31:02 +0100

    taking out unwanted urls

--- stockmgt/stock/urls.py
+++ stockmgt/stock/urls.py
@@ -15,21 +15,4 @@ urlpatterns = [
 
     path('display_switch', views.display_switch, name='display_switch'),
     path('display_accesspoint', views.display_Ap, name='display_accesspoint'),
-    path('display_powersupply', views.display_PowerSupply, name='display_powersupply'),
-
-    # url(r"^display_ap/$", display_Ap, name='display_Ap'),
-    # url(r"^display_powersupply/$", display_PowerSupply, name='display_PowerSupply'),
-
-# url(r"^add_switch$", add_switch, name='add_switch'),
-# url(r"^add_ap$", add_ap, name='add_ap'),
-# url(r"^add_power_supply$", add_power_supply, name='add_power_supply'),
-
-# url(r"^edit_switch/(?P<pk>\d+$", edit_switch, name='edit_switch'),
-# url(r"^edit_ap/(?P<pk>\d+$", edit_ap, name='edit_ap'),
-# url(r"^edit_power_supply/(?P<pk>\d+$", edit_power_supply, name='edit_power_suppply'),
-#
-#
-# url(r"^delete_power_supply/(?P<pk>\d+$", delete_switch, name='delete_switch'),
-# url(r"^delete_power_supply/(?P<pk>\d+$", delete_power_supply, name='delete_power_supply'),
-# url(r"^delete_power_supply/(?P<pk>\d+$", delete_ap, name='delete_ap'),
-]
+    path('display_powersupply', views.display_PowerSupply, name='display_powersupply')
