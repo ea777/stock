390764aa6ad36116544e000aa895e9fa6d33d086
eyob abraham <116373066@uamil.ucc.ie>
2019-08-08 11:22:03 +0100

    adding url paths to edit pages

--- stockmgt/stock/urls.py
+++ stockmgt/stock/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('display_accesspoint', views.display_accesspoint, name='display_accesspoint'),
     path('display_powersupply', views.display_powersupply, name='display_powersupply'),
     path('edit_switch/<int:id>', views.edit_switch, name='edit_switch'),
+    path('edit_accesspoint/<int:id>', views.edit_accesspoint, name='edit_accesspoint'),
+    path('edit_powersupply/<int:id>', views.edit_powersupply, name='edit_powersupply'),
     path('delete_switch', views.delete_switch, name='delete_switch'),
 	]
